319342c648d4b52fd9b606a111308b0baa555075
Kristen Arnold <karnold@dma.org>
2015-04-29 12:58:05 -0500

    Initial creation of plugin and additional database data

---
+++ Plugin.php
@@ -0,0 +1,80 @@
+<?php namespace DMA\LACMA;
+
+use System\Classes\PluginBase;
+use Illuminate\Support\Facades\Event;
+use DMA\Friends\Models\Usermeta as Metadata;
+
+/**
+ * LACMA Friends Plugin Information File
+ *
+ * @package DMA\LACMA
+ * @author Kristen Arnold
+ */
+class Plugin extends PluginBase
+{
+    /** 
+     * @var array Plugin dependencies
+     */
+    public $require = [
+        'DMA.Friends'
+    ];
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'LACMA Friends',
+            'description' => 'Customizations for LACMA NextGEN platform',
+            'author'      => 'Dallas Museum of Art',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function boot()
+    {
+        // Extend User fields
+        $context = $this;
+        Event::listen('backend.form.extendFields', function($widget) use ($context){
+            $context->extendedUserFields($widget);
+        }); 
+
+    }
+
+    /**
+     * Extend User fields in Rainlab.User plugin
+     * @param mixed $widget
+     */
+    private function extendedUserFields($widget)
+    {
+        if (!$widget->getController() instanceof \RainLab\User\Controllers\Users) return;
+        if ($widget->getContext() != 'update') return;
+        
+        // Make sure the User metadata exists for this user.
+        if (!Metadata::getFromUser($widget->model)) return;
+        
+        $widget->addFields([
+            'metadata[middle_name]' => [
+                'label' => 'Middle Name',
+                'tab'   => 'Metadata',
+                'span'  => 'left',
+            ],
+            'metadata[guardian_name]' => [
+                'label' => 'Parent/Guardian Full Name',
+                'tab'   => 'Metadata',
+                //'span'  => 'left',
+            ],
+            'metadata[expires_on]' => [
+                'label' => 'Expires On',
+                'tab'   => 'Metadata',
+                'span'  => 'left',
+            ],
+        ], 'primary');        
+    }
+}
---
+++ updates/add_user_data.php
@@ -0,0 +1,39 @@
+<?php namespace DMA\LACMA\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class AddUserData extends Migration {
+
+    /** 
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {   
+        Schema::table('dma_friends_usermetas', function($table)
+        {   
+            $table->string('guardian_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->timestamp('expires_on')->nullable();
+        });
+    }   
+
+
+    /** 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::table('dma_friends_usermetas', function($table)
+        {
+            $table->dropColumn('guardian_name');
+            $table->dropColumn('middle_name');
+            $table->dropColumn('expires_on');
+        });
+    }   
+
+}
